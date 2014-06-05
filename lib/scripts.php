b72a3fc1c4f9ad6161e31cc5f38101037cc8514c
tgelles <tgelles@pha.jhu.edu>
2014-06-05 13:43:36 -0400

    fixed 'Source Sans Pro' linking error in app.less file

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4c242e05f4244a7628e39f76abe73398');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ea88e30c55bfaeaedf9692cca6bfadb4');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
